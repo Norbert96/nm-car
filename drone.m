0e9491b3d716b96edd3041fe056113abe3242f42
csonthomisi <csontho.misi@gmail.com>
2019-03-13 11:51:36 +0100

    Files to start this project
    Matlab file
    Excel file for the references

---
+++ drone.m
@@ -0,0 +1,13 @@
+% Drone: P4 S-mode
+v_ascending=6;      %[m/s]
+v_horizontal=20;    %[m/s]
+max_tilt=42;        %[deg]
+max_tilt_rad=deg2rad(max_tilt);
+weight=1.38;        %[kg]
+
+
+
+
+% Drone model
+% MAP model
+% Optimalization
