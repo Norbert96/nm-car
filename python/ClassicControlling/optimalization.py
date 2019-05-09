f81c28a9de912e809e7acb73599f5dde6b34c948
Paufler Norbert <paufler.norbert@gmail.com>
2019-05-09 15:58:27 +0200

    is finished with line step through

--- python/ClassicControlling/optimalization.py
+++ python/ClassicControlling/optimalization.py
@@ -5,21 +5,30 @@ import numpy as np
 
 
 class OptimalizationControllAgent(ClassicControll):
-    def __init__(self, map_json):
+    def __init__(self, map_json,max_distance_from_middle=20):
         ClassicControll.__init__(self, map_json)
         self.speed_section_num = 100
         self.initial_speed = 5
         self.speed_delta = 1
-        self.map = OptimalizationMap(map_json, 10)
+        self.max_distance_from_middle = max_distance_from_middle
+        self.map = OptimalizationMap(map_json,self.max_distance_from_middle )
         self.round = 0
         self.i = 0
         self.rabbit_speed = 15
+
+        self.out_start_zone = False
+        self.last_distance_from_finish_line = None
 
     def start_optimalization(self):
         self.section_speed = [self.initial_speed] * self.speed_section_num
 
         while True:
             self.run(0.1)
+
+    def res(self):
+        self.out_start_zone = False
+        self.last_distance_from_finish_line = None
+
 
     def run(self, t):
         self.drone.controll(self.calculate_controlling_parameters())
@@ -32,6 +41,28 @@ class OptimalizationControllAgent(ClassicControll):
         #     print(self.i)
 
         self.map.point_on_route(self.drone.position)
+        print(self.is_finished())
+
+
+    def is_finished(self):
+        distance_from_finish_line, distance_from_finish_point = self.map.distance_from_finish_line_and_point(self.drone.position)
+        if distance_from_finish_point > self.max_distance_from_middle:
+            self.out_start_zone = True
+
+        if self.out_start_zone:
+            if self.max_distance_from_middle > distance_from_finish_line: #in start zone again
+                if self.last_distance_from_finish_line is not None:
+                    if self.last_distance_from_finish_line * distance_from_finish_line < 0.0:
+                        return True
+                self.last_distance_from_finish_line = distance_from_finish_line
+
+        return False
+
+
+
+
+
+
 
 
 class OptimalizationMap(Map):
@@ -39,12 +70,34 @@ class OptimalizationMap(Map):
         Map.__init__(self, map_file)
         self.max_distance_from_midline = distance_from_midline
         self.path_line_points = self.create_line_points()
+        self.middle_line_points = self.create_middle_line_points()
+        self.finish_line = self.create_finish_line()
 
     def draw_map(self, frame):
         frame = self.draw_map_line(frame)
         for bezier in self.curve_list:
             draw_line_from_distance(frame, bezier, self.max_distance_from_midline)
             draw_line_from_distance(frame, bezier, -self.max_distance_from_midline)
+
+    def create_finish_line(self):
+        x1, y1 = self.curve_list[0].get_bezier_points_offseted(10)
+        x2, y2 = self.curve_list[0].get_bezier_points_offseted(-10)
+        line = np.array([[x1[0],y1[0]], [x2[0], y2[0]]])
+        return line
+
+    def distance_from_finish_line_and_point(self, drone_position):
+        p1 = self.finish_line[0]
+        p2 = self.finish_line[1]
+        p3 = drone_position
+
+        distance_from_finish_line = np.cross(p2 - p1, p3 - p1) / np.linalg.norm(p2 - p1)
+        starting_point = self.curve_list[0].get(0)
+        distance_from_finish_point = np.linalg.norm(drone_position - starting_point)
+
+        return distance_from_finish_line, distance_from_finish_point
+
+
+
 
     def create_line_points(self, resolution=100):
         delta = self.path_len / resolution
@@ -57,6 +110,14 @@ class OptimalizationMap(Map):
         p1 = np.array(p1)
         p2 = np.array(p2)
         return np.array([p1, p2])
+
+    def create_middle_line_points(self, resolution=1000):
+        delta = self.path_len / resolution
+        p = []
+        for i in range(resolution):
+            p.append(self.get_point_distance_from_start(delta * i))
+        return np.array(p)
+
 
     def draw_path_line_points(self, frame):
         import cv2
@@ -137,9 +198,11 @@ class OptimalizationMap(Map):
         return DistancePointLine
 
     def point_on_route(self, point):
-        closest_dist = self.line_distance_from_point(self.path_line_points[0], self.path_line_points[1], point).min()
-        print(closest_dist)
-        return closest_dist < self.max_distance_from_midline
+        #closest_dist1 = self.line_distance_from_point(self.path_line_points[0], self.path_line_points[1], point).min()
+        closest_dist2 = np.linalg.norm(self.middle_line_points-point, axis=1).min()
+        #print('By line: ' + str(closest_dist1))
+        print('By point: ' + str(closest_dist2))
+        return closest_dist2 < self.max_distance_from_midline
 
     def in_which_speed_section(self, section_number):
         dist_from_start = self.current_distance % self.path_len
